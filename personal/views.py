ee062dfe073600520878a3be30a41fb666458896
imklesley <imklesley@gmail.com>
2020-10-27 12:54:35 -0300

    Inicio do desenvolvimento do Blog

---
+++ personal/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+
+def home_screen_view(request):
+    print(request.headers)
+    return render(request=request, template_name='personal/home.html', context={})
