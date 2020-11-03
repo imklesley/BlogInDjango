20eb7dcd4e846fc90c95c2c328dc7f8714d40a44
imklesley <imklesley@gmail.com>
2020-11-03 00:05:00 -0300

    Apaguei secret key - github alert

--- BlogInDjango/settings.py
+++ BlogInDjango/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't&xvb*7+c22v2(tt8l$u%rolw78)8dt%n@l-ph#u2^uxoq0p1v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
